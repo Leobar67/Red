4554d187e7f8d4c47ae388794c8bd698a6acd00c
Ros-spec <rjge022604@gmail.com>
2025-06-24 01:36:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,29 +7,12 @@ if(isset($_GET['sensor_port'])){
 $con = new BD_PDO();
 $consumo =  @$_GET['consumo']; $temp = @$_GET['temp']; $hora = date('H:i:s');
 
-if(!$consumo || !$temp){
-    echo json_encode(["status" => "Los campos estan vacios"]);
-    exit;
-}else{
-    $insertar = $con ->Ejecutar_Instruccion("INSERT into Sensores(Consumo, Temperatura, Hora) values ('$consumo', '$temp', '$hora')"); 
-    echo json_encode($insertar); 
-    exit;
-}
-
-}
-
-if(isset($_GET['cambios'])){
-    $con = new BD_PDO();
-    $result = $con -> Ejecutar_Instruccion("UPDATE Sensores set Consumo = '2.64' where Id_Datos = 1");
-}
-
-if(isset($_GET['show'])){
-    $con = new BD_PDO();
-    // $con -> Ejecutar_Instruccion("ALTER TABLE Sensores MODIFY Id_Datos INT NOT NULL AUTO_INCREMENT");
-
-    $result = $con->Ejecutar_Instruccion("SHOW CREATE TABLE Sensores");
-    echo json_encode($result);
-    exit;
+    if(!$consumo || !$temp){
+        echo json_encode(["status" => "Los campos estan vacios"]); exit;
+    }else{
+        $insertar = $con ->Ejecutar_Instruccion("INSERT into Sensores(Consumo, Temperatura, Hora) values ('$consumo', '$temp', '$hora')"); 
+        echo json_encode($insertar); exit;
+    }
 }
 
 if(isset($_GET['datos'])){
