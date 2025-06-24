6dfbcc4b3ca33dee190fb5cfd11a0ab8d56b9ee5
Ros-spec <rjge022604@gmail.com>
2025-06-24 17:16:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json");
 if(isset($_GET['sensor_port'])){
 $con = new BD_PDO();
 $consumo =  @$_GET['consumo']; $temp = @$_GET['temp']; $hora = date('H:i:s');
-
+$consuno = $consumo * (-1);
     if(!$consumo || !$temp){
         echo json_encode(["status" => "Los campos estan vacios"]); exit;
     }else{
