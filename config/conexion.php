03904675ddcec5c87aead65125f6c2d33577e913
Ros-spec <rjge022604@gmail.com>
2025-06-23 22:40:35 -0500

    Update conexion.php
    nuevo, ya no mover

--- config/conexion.php
+++ config/conexion.php
@@ -1,19 +1,35 @@
 <?php
-$host = "localhost";           // Servidor
-$baseDeDatos = "gregario-sorpresa";      // Base de datos
-$usuario = "root";             // Usuario
-$contrasena = "";              // Contraseña
 
-try {
-    $conexion = new PDO("mysql:host=$host;dbname=$baseDeDatos;charset=utf8", $usuario, $contrasena);
-    
-    // Habilitar excepciones en caso de error
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    echo "✅ Conexión exitosa";
-} catch (PDOException $e) {
-    echo "❌ Error de conexión: " . $e->getMessage();
+class BD_PDO {
+	function Ejecutar_Instruccion($instruccion_sql){
+	$host = "caboose.proxy.rlwy.net";
+		$port = 53956;
+		$usr  = "root";
+		$pwd  = "rUiNQxWUAAfGKqMaBKgvqzHEaYtuzSnO";
+		$db   = "railway";
+
+		try {
+			$conexion = new PDO("mysql:host=$host;port=$port;dbname=$db;", $usr, $pwd);
+			// $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			echo "✅ Conexión exitosa a Railway";
+        }
+		catch(PDOException $e){
+		      echo "Failed to get DB handle: " . $e->getMessage();
+		      exit;    
+		    }
+		 
+		 // Asignando una instruccion sql
+		 $query=$conexion->prepare($instruccion_sql);
+		 
+		if(!$query){
+			return "Error al mostrar";
+		}else{
+			$query->execute();
+			while ($result = $query->fetch())
+			    {
+			        $rows[] = $result;
+			    }	
+		} return @$rows;
+	}
 }
 ?>
-
-
