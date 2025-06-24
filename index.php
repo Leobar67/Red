60d2057f73c02f9602d10376b1e22e6bcce7e3fc
Ros-spec <rjge022604@gmail.com>
2025-06-23 23:56:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
-<?php
-require 'config/conexion.php';
+<?php  require_once 'config/conexion.php'; 
+date_default_timezone_set("America/Monterrey");
+echo date('H:i:s');
 
-$bd = new BD_PDO();
-$resultado = $bd->Ejecutar_Instruccion("SELECT * FROM Sensores");
+if(isset($_GET['sensor_port'])){
+$con = new BD_PDO();
+$consumo =  @$_GET['consumo']; $temp = @$_GET['temp']; $fecha = date('H:i:s');
 
-echo "✅ API funcionando.<br>";
-echo "<pre>";
-print_r($resultado);
-echo "</pre>";
+if(!$consumo || !$temp){
+    echo 'Los campos estan vacios <br>';
+}else{
+    try{
+        $con->Ejecutar_Instruccion("ALTER TABLE Sensores MODIFY Id_Datos INT AUTO_INCREMENT PRIMARY KEY");
+    }catch(PDOException $e){}
+    $insertar = $con ->Ejecutar_Instruccion("INSERT into Sensores(Consumo, Temperatura, Fechahora) values ('$consumo', '$temp', '$fecha')"); 
+}
+
+}
+
+if(isset($_GET['cambios'])){
+    $con = new BD_PDO();
+
+    $fecha = date('H:i:s');
+    $result = $con -> Ejecutar_Instruccion("UPDATE Sensores set Hora = '{$fecha}' where Id_Datos = 1");
+    echo($result);
+}
+
+
+if(isset($_GET['datos'])){
+    $con = new BD_PDO();
+    @$id = $_GET['id'];
+
+    if(isset($id)){
+        $select = $con -> Ejecutar_Instruccion("SELECT * from Sensores where Id_datos = '{$id}' ");
+    }else{
+        $select = $con -> Ejecutar_Instruccion("SELECT * from Sensores");
+    }
+
+    var_dump($select);
+}
+
 ?>
