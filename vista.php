12f6231b58ddaf15c4db3ae2faca74b9d27b06e6
Leobar67 <158235574+Leobar67@users.noreply.github.com>
2025-06-23 21:52:24 -0600

    Update vista.php

--- vista.php
+++ vista.php
@@ -1,3 +1,9 @@
+<?php
+require 'config/conexion.php';
+$bd = new BD_PDO();
+$datos = $bd->Ejecutar_Instruccion("SELECT * FROM Sensores");
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,7 +18,7 @@
         <input type="submit" value="Agregar">
     </form>
 
-    <h2>Datos de sensores</h2>
+    <h2>Datos existentes</h2>
     <table border="1">
         <tr>
             <th>ID</th>
@@ -22,32 +28,27 @@
             <th>Acciones</th>
         </tr>
 
-        <?php
-        require 'config/conexion.php';
-        $result = $conn->query("SELECT * FROM Sensores");
-
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>
-                <td>{$row['Id_Datos']}</td>
-                <td>{$row['Consumo']}</td>
-                <td>{$row['Temperatura']}</td>
-                <td>{$row['Fechahora']}</td>
+        <?php foreach ($datos as $row): ?>
+            <tr>
+                <td><?= $row['Id_Datos'] ?></td>
+                <td><?= $row['Consumo'] ?></td>
+                <td><?= $row['Temperatura'] ?></td>
+                <td><?= $row['Fechahora'] ?></td>
                 <td>
                     <form action='eliminar.php' method='GET' style='display:inline;'>
-                        <input type='hidden' name='id' value='{$row['Id_Datos']}'>
+                        <input type='hidden' name='id' value='<?= $row['Id_Datos'] ?>'>
                         <input type='submit' value='Eliminar'>
                     </form>
                     <form action='editar.php' method='POST' style='display:inline;'>
-                        <input type='hidden' name='id' value='{$row['Id_Datos']}'>
-                        <input type='number' name='consumo' value='{$row['Consumo']}' step='0.01' required>
-                        <input type='number' name='temperatura' value='{$row['Temperatura']}' step='0.01' required>
-                        <input type='datetime-local' name='fechahora' value='" . date('Y-m-d\TH:i', strtotime($row['Fechahora'])) . "' required>
+                        <input type='hidden' name='id' value='<?= $row['Id_Datos'] ?>'>
+                        <input type='number' name='consumo' value='<?= $row['Consumo'] ?>' step='0.01' required>
+                        <input type='number' name='temperatura' value='<?= $row['Temperatura'] ?>' step='0.01' required>
+                        <input type='datetime-local' name='fechahora' value='<?= date('Y-m-d\TH:i', strtotime($row['Fechahora'])) ?>' required>
                         <input type='submit' value='Editar'>
                     </form>
                 </td>
-            </tr>";
-        }
-        ?>
+            </tr>
+        <?php endforeach; ?>
     </table>
 </body>
 </html>
