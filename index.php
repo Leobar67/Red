ff8245c316afcc72e947db813fef7167679559a0
Ros-spec <rjge022604@gmail.com>
2025-06-23 23:58:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,24 +9,18 @@ $consumo =  @$_GET['consumo']; $temp = @$_GET['temp']; $fecha = date('H:i:s');
 if(!$consumo || !$temp){
     echo 'Los campos estan vacios <br>';
 }else{
-    try{
-        $con->Ejecutar_Instruccion("ALTER TABLE Sensores MODIFY Id_Datos INT AUTO_INCREMENT PRIMARY KEY");
-    }catch(PDOException $e){}
-    $insertar = $con ->Ejecutar_Instruccion("INSERT into Sensores(Consumo, Temperatura, Fechahora) values ('$consumo', '$temp', '$fecha')"); 
+    try{ $con->Ejecutar_Instruccion("ALTER TABLE Sensores MODIFY Id_Datos INT AUTO_INCREMENT PRIMARY KEY");    }
+    catch(PDOException $e){}
+        $insertar = $con ->Ejecutar_Instruccion("INSERT into Sensores(Consumo, Temperatura, Fechahora) values ('$consumo', '$temp', '$fecha')"); 
+    } 
 }
 
-}
-
-if(isset($_GET['cambios'])){
-    $con = new BD_PDO();
-
-    $fecha = date('H:i:s');
+elseif(isset($_GET['cambios'])){
+    $con = new BD_PDO(); $fecha = date('H:i:s');
     $result = $con -> Ejecutar_Instruccion("UPDATE Sensores set Hora = '{$fecha}' where Id_Datos = 1");
     echo($result);
 }
-
-
-if(isset($_GET['datos'])){
+elseif(isset($_GET['datos'])){
     $con = new BD_PDO();
     @$id = $_GET['id'];
 
@@ -35,8 +29,6 @@ if(isset($_GET['datos'])){
     }else{
         $select = $con -> Ejecutar_Instruccion("SELECT * from Sensores");
     }
-
     var_dump($select);
-}
-
-?>
+} 
+    ?>
