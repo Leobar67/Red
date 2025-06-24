2da63ea11802822103237bff621d6d366e96c351
Leobar67 <158235574+Leobar67@users.noreply.github.com>
2025-06-23 21:46:07 -0600

    Update eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,14 +1,15 @@
 <?php
 require 'config/conexion.php';
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+$id = $_GET['id'];
 
-    $deleteQuery = "DELETE FROM Tiempo WHERE Id_Datos = $id";
-    if (mysqli_query($conexion, $deleteQuery)) {
-        header("Location: index.php");  // Redirigir a la página principal
-    } else {
-        echo "Error: " . mysqli_error($conexion);
-    }
+$sql = "DELETE FROM Sensores WHERE Id_Datos = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    echo "Dato eliminado.";
+} else {
+    echo "Error al eliminar: " . $conn->error;
 }
 ?>
